a00345f317d1717eda64f28b78c78de157913e4c
joel.pettersson90@gmail.com <joel.pettersson90@gmail.com>
2022-01-04 08:52:24 +0100

    second try, and made it better with drop down menu for week days menu

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const theDay = document.getElementById("theDay");
 const onTheMenu = document.getElementById("onTheMenu");
+let mealDiv = document.getElementById("mealDiv");
+let meal = "";
 
 let weekdays = [
   "Söndag",
@@ -51,12 +53,12 @@ switch (day) {
     console.log("Ärtsoppa och pannkakor");
     break;
   case 5:
-    text = "Ärtsoppa och pannkakor";
-    console.log("Ärtsoppa och pannkakor");
-    break;
-  case 6:
     text = "Tacos";
     console.log("Tacos");
+    break;
+  case 6:
+    text = "Stängt";
+    console.log("Stängt");
     break;
 
   default:
@@ -74,3 +76,52 @@ weekdays.forEach((day) => {
   dayListItem.id = day;
   dayList.append(dayListItem);
 });
+
+let checkDay = () => {
+  let dayList = document.getElementById("dayList").value;
+  mealDiv.innerHTML = "";
+
+  switch (dayList) {
+    case "Måndag":
+      meal = "Köttbullar, potatis och sås";
+      displayMeal(meal);
+      break;
+    case "Tisdag":
+      meal = "Fiskpanetter, potatismos och remuladsås";
+      displayMeal(meal);
+      break;
+    case "Onsdag":
+      meal = "Blodpudding med lingon";
+      displayMeal(meal);
+      break;
+    case "Torsdag":
+      meal = "Ärtsoppa och pannkakor";
+      displayMeal(meal);
+      break;
+    case "Fredag":
+      meal = "Tacos";
+      displayMeal(meal);
+      break;
+    case "Lördag":
+      meal = "Ingen mat idag då det tyvärr är stängt";
+      displayMeal(meal);
+      break;
+    case "Söndag":
+      meal = "Inte ens på guds dag serveras det mat!?";
+      displayMeal(meal);
+      break;
+
+    default:
+      meal = "Välj dag";
+      displayMeal(meal);
+      break;
+  }
+};
+
+let button = document.getElementById("btn");
+button.addEventListener("click", checkDay);
+
+let displayMeal = (meal) => {
+  console.log(meal);
+  mealDiv.append(meal);
+};
